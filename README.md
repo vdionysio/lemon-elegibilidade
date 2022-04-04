4cfbeea44a019a4e8e07ff90dca448a138056ee3
vdionysio <vini.dionysio@gmail.com>
2022-04-04 07:20:16 -0300

    feat: adiciona doc simples na rota /doc

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 API Interna para checagem de elegibilidade
 
-docker build -t vdionysio/lemon-eligibility .
+cd lemon-elegibilidade
+
+Para rodar com Docker
+docker-compose up
+
+
+Para rodar sem Docker
+npm install
+npm start
+
+http://localhost:5000/
+
+http://ec2-18-229-124-142.sa-east-1.compute.amazonaws.com:5000/doc
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 const { error } = require('./middlewares');
 
@@ -13,6 +14,11 @@ app.use('/eligibility', routes.eligibility);
 app.get('/', (req, res) => {
   res.send('Lemon energia API');
 });
+
+app.get('/doc', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
 app.use(error);
 
 app.listen(5000, () => {
